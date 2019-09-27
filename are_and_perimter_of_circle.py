9da0b0a9e78a84b4cfcc3c412fee0319d35147be
Ubuntu <ubuntu@ip-172-31-29-44.ec2.internal>
2019-09-27 22:00:40 +0000

    calculate area and perimeter of a circle

---
+++ are_and_perimter_of_circle.py
@@ -0,0 +1,17 @@
+  #!usr/bin/env python3
+
+# Created by: Dylan Hanna
+# Created on: Sept 2019
+# This program calculates the area and perimeter of a circle with the radius of 15mm
+
+import math
+
+def main():
+    print("If a circle has a radius of 15mm:")
+    print("")
+    print("Area is {}mm^2".format(math.pi*15**2))
+    print("Perimeter is {}mm".format(2*math.pi*15))
+    
+    
+if __name__ == "__main__":
+   main()
---
+++ math.py
@@ -0,0 +1,7 @@
+#!usr/bin/env python3
+
+# Created by: Dylan Hanna
+# Created on: Sept 2019
+# This program holds the equations for the area and perimeter of a circle
+
+math_pi = 3.14
